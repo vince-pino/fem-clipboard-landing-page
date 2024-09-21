ba1cc350664e4a974b196e7e4c74dc250e5da274
Vince Cedie Pino <vincecediepino@gmail.com>
2024-09-21 10:12:10 +0700

    style header el

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
       fontFamily: {
         sans: ["Bai Jamjuree", "sans-serif"],
       },
+      colors: {
+        strongCyan: "hsl(171, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+      },
     },
   },
   plugins: [],
